2cfb8a1c6bf24f12e1cd3a1f9efa692b8f9cefb3
Karan Sharma <sharma.karan9341@gmail.com>
2018-11-02 12:10:14 -0400

    Added New Authentication controller

---
+++ ReDataViz/Controllers/AuthenticationController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace ReDataViz.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        [HttpPost("register")]
+        public void Register()
+        {
+            var jwt = new JwtSecurityToken();
+            var encoded_jwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+        }
+    }
+}
